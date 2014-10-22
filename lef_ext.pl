2c95c4999e1f3887ac968f9d2f838b9b7df25c93
bob-icsg <bobzhou@bu.edu>
2014-10-22 14:54:46 -0400

    AUTOCOMMIT bobzhou@celnode04:Wed Oct 22 14:00:09 EDT 2014

--- lef_ext.pl
+++ lef_ext.pl
@@ -4,63 +4,60 @@ use File::Basename ;
 use integer ;
 use warnings ;
 
-$lefpre     = "FILLCELL\_X" ;
+$lefpre     = "lef\\" ;
 $lefsuf     = "\.lef" ;
+$lefcell    = $lefpre ;
 $lefname    = $lefpre ;
-#$cmd        = "pwd\n" ;
-$size       = 1 ;
-$XorY       = 0 ;
 
-$output     = "des\_FILLCELL.txt" ;
+$output     = "des\_ALLCELL.txt" ;
 #write to file
 open $writeFile , "+>" , $output or die "$output is not available!\n" ; 
 print $output . " has been successfully opened!\n" ;
 
-for ( my $i = 1 ; $i <= 32 ; $i = $i * 2 ) {
-    #cellname creation
-    $size       = $i ; 
-    $lefname    = $lefpre . $size . $lefsuf ; 
+#cellname creation
+while ( <$lefpre . /*/ . $lefsuf> ) {
+    $lefcell    = "$&" ;
+    $lefname    = $lefpre . $lefcell . $lefsuf ; 
 
-    #read file
-    open $readFile , "<" , $lefname or die "$lefname is not available!\n" ;
-    print $lefname . " has been successfully opened!\n" ;
+#read file
+open $readFile , "<" , $lefname or die "$lefname is not available!\n" ;
+print $lefname . " has been successfully opened!\n" ;
 
-    #write the name of the cell
-    print $writeFile $lefname . "\n" ;
+#write the name of the cell
+print $writeFile $lefname . "\n" ;
 
-    #write info the cell
-    while ( <$readFile> ) {
-        if ( /SIZE*/ ) {
-            print $writeFile "$&\n" ;
-            #matching a floating number [-+]?([0-9]*\.[0-9]+|[0-9]+)
-            while ( /[-+]?([0-9]*\.[0-9]+|[0-9]+)/g ) {
-                if ( $XorY == 0 ) {
-                    print $writeFile "X $&\t" ;
-                    $XorY   = 1 ;
-                } else {
-                    print $writeFile "Y $&\n" ;
-                    $XorY   = 0 ;
-                }
-            }
-        }
-        if ( /POLYGON*/ ) {
-            print $writeFile "$&\n" ;
-            while ( /[-+]?([0-9]*\.[0-9]+|[0-9]+)/g ) {
-                if ( $XorY == 0 ) {
-                    print $writeFile "X $&\t" ;
-                    $XorY   = 1 ;
-                } else {
-                    print $writeFile "Y $&\n" ;
-                    $XorY   = 0 ;
-                }
+#write info the cell
+while ( <$readFile> ) {
+    if ( /SIZE*/ ) {
+        print $writeFile "$&\n" ;
+        #matching a floating number [-+]?([0-9]*\.[0-9]+|[0-9]+)
+        while ( /[-+]?([0-9]*\.[0-9]+|[0-9]+)/g ) {
+            if ( $XorY == 0 ) {
+                print $writeFile "X $&\t" ;
+                $XorY   = 1 ;
+            } else {
+                print $writeFile "Y $&\n" ;
+                $XorY   = 0 ;
             }
         }
     }
-
-    #end of the one readFile
-    print $writeFile "\n" ;
-    close ( $readFile ) ;
+    if ( /POLYGON*/ ) {
+        print $writeFile "$&\n" ;
+        while ( /[-+]?([0-9]*\.[0-9]+|[0-9]+)/g ) {
+            if ( $XorY == 0 ) {
+                print $writeFile "X $&\t" ;
+                $XorY   = 1 ;
+            } else {
+                print $writeFile "Y $&\n" ;
+                $XorY   = 0 ;
+            }
+        }
+    }
 }
 
+#end of the one readFile
+print $writeFile "\n" ;
+
+close ( $readFile ) ;
 close ( $writeFile ) ;
 
