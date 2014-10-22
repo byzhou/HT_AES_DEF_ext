119b4ea9a3d47aed9a948b2073fdb151962b55f5
bob-icsg <bobzhou@bu.edu>
2014-10-22 15:17:15 -0400

    read files works

--- lef_ext.pl
+++ lef_ext.pl
@@ -3,61 +3,62 @@
 use File::Basename ;
 use integer ;
 use warnings ;
+use strict ;
 
-$lefpre     = "lef\\" ;
-$lefsuf     = "\.lef" ;
-$lefcell    = $lefpre ;
-$lefname    = $lefpre ;
+my $lefpre     = "lef\/" ;
+my $lefsuf     = "\.lef" ;
+my $lefcell    = $lefpre ;
+my $lefname    = $lefpre ;
+my $readFile ;
+my $writeFile ;
+my $XorY ;
 
-$output     = "des\_ALLCELL.txt" ;
+my $output     = "des\_ALLCELL.txt" ;
+
 #write to file
 open $writeFile , "+>" , $output or die "$output is not available!\n" ; 
 print $output . " has been successfully opened!\n" ;
 
+#open directory
+opendir ( DIR , $lefpre ) or die "$lefpre is not available!\n" ;
+print $lefpre . " has been successfully opened!\n" ;
+close ( DIR ) ;
+
 #cellname creation
-while ( <$lefpre . /*/ . $lefsuf> ) {
-    $lefcell    = "$&" ;
-    $lefname    = $lefpre . $lefcell . $lefsuf ; 
+while ( $lefcell = readdir ( DIR ) ) {
+    
+    #define cellname
+    $lefname    = $lefcell . $lefsuf ; 
+    print $lefname . "\n" ;
 
-#read file
-open $readFile , "<" , $lefname or die "$lefname is not available!\n" ;
-print $lefname . " has been successfully opened!\n" ;
-
-#write the name of the cell
-print $writeFile $lefname . "\n" ;
-
-#write info the cell
-while ( <$readFile> ) {
-    if ( /SIZE*/ ) {
-        print $writeFile "$&\n" ;
-        #matching a floating number [-+]?([0-9]*\.[0-9]+|[0-9]+)
-        while ( /[-+]?([0-9]*\.[0-9]+|[0-9]+)/g ) {
-            if ( $XorY == 0 ) {
-                print $writeFile "X $&\t" ;
-                $XorY   = 1 ;
-            } else {
-                print $writeFile "Y $&\n" ;
-                $XorY   = 0 ;
-            }
-        }
-    }
-    if ( /POLYGON*/ ) {
-        print $writeFile "$&\n" ;
-        while ( /[-+]?([0-9]*\.[0-9]+|[0-9]+)/g ) {
-            if ( $XorY == 0 ) {
-                print $writeFile "X $&\t" ;
-                $XorY   = 1 ;
-            } else {
-                print $writeFile "Y $&\n" ;
-                $XorY   = 0 ;
-            }
-        }
-    }
+    #read file
+#    open $readFile , "<" , $lefname or die "$lefname is not available!\n" ;
+#    print $lefname . " has been successfully opened!\n" ;
+#
+#    #write the name of the cell
+#    print $writeFile $lefname . "\n" ;
+#
+#    #write info the cell
+#    while ( <$readFile> ) {
+#        if ( /SIZE*/ ) {
+#            print $writeFile "$&\n" ;
+#            #matching a floating number [-+]?([0-9]*\.[0-9]+|[0-9]+)
+#            while ( /[-+]?([0-9]*\.[0-9]+|[0-9]+)/g ) {
+#                if ( $XorY == 0 ) {
+#                    print $writeFile "X $&\t" ;
+#                    $XorY   = 1 ;
+#                } else {
+#                    print $writeFile "Y $&\n" ;
+#                    $XorY   = 0 ;
+#                }
+#            }
+#        }
+#    }
+#
+#    #end of the one readFile
+#    close ( $readFile ) ;
+#    print $writeFile "\n" ;
 }
 
-#end of the one readFile
-print $writeFile "\n" ;
-
-close ( $readFile ) ;
 close ( $writeFile ) ;
 
